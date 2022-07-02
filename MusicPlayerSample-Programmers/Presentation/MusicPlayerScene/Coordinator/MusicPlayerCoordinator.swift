e6a58bff77a3f33b9a856869c453a578745f7b3a
Jaewon <woin2ee@gmail.com>
2022-07-03 03:37:50 +0900

    feat : 화면 전환 애니메이션 구현을 위한 기반 구성

--- MusicPlayerSample-Programmers/Presentation/MusicPlayerScene/Coordinator/MusicPlayerCoordinator.swift
+++ MusicPlayerSample-Programmers/Presentation/MusicPlayerScene/Coordinator/MusicPlayerCoordinator.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import SnapKit
 
-final class MusicPlayerCoordinator: Coordinator {
+final class MusicPlayerCoordinator: NSObject, Coordinator {
     
     weak var navigationController: UINavigationController?
     
@@ -18,6 +19,42 @@ final class MusicPlayerCoordinator: Coordinator {
     func start() {
         let musicPlayerVM = DefaultMusicPlayerViewModel(coordinator: self)
         let musicPlayerVC = MusicPlayerViewController(viewModel: musicPlayerVM)
+        
+        navigationController?.delegate = self
         navigationController?.setViewControllers([musicPlayerVC], animated: false)
+    }
+}
+
+extension MusicPlayerCoordinator: UINavigationControllerDelegate {
+    func navigationController(
+        _ navigationController: UINavigationController,
+        animationControllerFor operation: UINavigationController.Operation,
+        from fromVC: UIViewController,
+        to toVC: UIViewController
+    ) -> UIViewControllerAnimatedTransitioning? {
+        return MyAnimator()
+    }
+}
+
+final class MyAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 1
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let containerView = transitionContext.containerView
+        guard let toView = transitionContext.view(forKey: .to) else { return }
+        
+        containerView.addSubview(toView)
+
+        toView.snp.makeConstraints { make in
+            make.top.bottom.leading.trailing.equalToSuperview()
+        }
+        
+        if transitionContext.isAnimated {
+        }
+        
+        transitionContext.completeTransition(true)
     }
 }
