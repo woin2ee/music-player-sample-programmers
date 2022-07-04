44efc2bca33f5440f20c71fda4e67af97d9a7af8
Jaewon <woin2ee@gmail.com>
2022-07-04 17:03:23 +0900

    feat : Add Music Entity

---
+++ MusicPlayerSample-Programmers/Domain/Entities/Music.swift
@@ -0,0 +1,20 @@
+//
+//  Music.swift
+//  MusicPlayerSample-Programmers
+//
+//  Created by Jaewon on 2022/07/04.
+//
+
+import UIKit
+
+struct Music {
+    typealias Lyrics = [Date:String]
+    
+    let title: String
+    let singer: String
+    let albumTitle: String
+    let albumImage: UIImage
+    let file: Data
+    let lyrics: Lyrics
+    let duration: Int
+}
