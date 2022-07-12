2c73d40d9a857cd5573d77599b52f75c15eda3a1
Jaewon <woin2ee@gmail.com>
2022-07-12 17:41:11 +0900

    test : index 가 -1이 되는 케이스로 인해 로직 수정

--- MusicPlayerSample-ProgrammersTests/LyricsTableViewTests.swift
+++ MusicPlayerSample-ProgrammersTests/LyricsTableViewTests.swift
@@ -21,7 +21,7 @@ class LyricsTableViewTests: XCTestCase {
         let currentPlayTime: TimeInterval = 17
         
         // when
-        let currentLyricsIndex: Int = (playTimeArr.firstIndex(where: { $0 > Float(currentPlayTime) }) ?? 1) - 1
+        let currentLyricsIndex: Int = playTimeArr.lastIndex(where: { $0 < Float(currentPlayTime) }) ?? 0
         
         // then
         XCTAssertEqual(currentLyricsIndex, 0)
@@ -34,11 +34,24 @@ class LyricsTableViewTests: XCTestCase {
         let currentPlayTime: TimeInterval = 22
         
         // when
-        let currentLyricsIndex: Int = (playTimeArr.firstIndex(where: { $0 > Float(currentPlayTime) }) ?? 1) - 1
+        let currentLyricsIndex: Int = playTimeArr.lastIndex(where: { $0 < Float(currentPlayTime) }) ?? 0
         
         // then
         XCTAssertNotEqual(currentLyricsIndex, 1)
         XCTAssertEqual(currentLyricsIndex, 2)
         XCTAssertNotEqual(currentLyricsIndex, 3)
+    }
+    
+    func test_currentLyricsIndex3() throws {
+        // given
+        let playTimeArr: [Float] = [16.2, 18.3, 21.1, 23.6, 26.3, 28.7, 31.4, 33.6, 36.5, 38.9, 41.5, 44.2, 46.6, 49.3, 52.2, 54.5, 57.0, 59.7, 62.1, 64.8, 67.4, 70.0, 72.5, 75.0, 77.7, 80.2, 82.8, 85.3, 89.8, 131.9, 134.0, 136.5, 139.4, 142.0, 144.4, 147.0, 149.6, 152.2, 154.5, 157.2, 160.0, 162.4, 165.0, 167.6, 170.2, 172.6, 175.3, 177.9, 180.5]
+        let currentPlayTime: TimeInterval = 0
+        
+        // when
+        let currentLyricsIndex: Int = playTimeArr.lastIndex(where: { $0 < Float(currentPlayTime) }) ?? 0
+        
+        // then
+        XCTAssertEqual(currentLyricsIndex, 0)
+        XCTAssertNotEqual(currentLyricsIndex, 1)
     }
 }
