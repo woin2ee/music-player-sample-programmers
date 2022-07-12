27beca2186a547524d887a1d96ed96a06179edbd
Jaewon <woin2ee@gmail.com>
2022-07-12 15:57:04 +0900

    feat : String extension 에 가사 computed property 추가

--- MusicPlayerSample-Programmers/Extension/String+.swift
+++ MusicPlayerSample-Programmers/Extension/String+.swift
@@ -7,10 +7,41 @@
 
 import Foundation
 
+enum LyricsIndex: Int {
+    case minuteStart = 1
+    case minuteEnd = 2
+    case secondStart = 4
+    case secondEnd = 5
+    case millisecond = 7
+    case lyricsStart = 11
+}
+
 extension String {
-    func slice(from: Int, to: Int) -> String {
-        let startIndex = self.index(self.startIndex, offsetBy: from)
-        let endIndex = self.index(self.startIndex, offsetBy: to)
+    func slice(from: LyricsIndex, to: LyricsIndex) -> String {
+        let startIndex = self.index(self.startIndex, offsetBy: from.rawValue)
+        let endIndex = self.index(self.startIndex, offsetBy: to.rawValue)
         return String(self[startIndex...endIndex])
+    }
+    
+    func slice(from: LyricsIndex) -> String {
+        let startIndex = self.index(self.startIndex, offsetBy: from.rawValue)
+        let endIndex = self.endIndex
+        return String(self[startIndex..<endIndex])
+    }
+    
+    var duration: Float {
+        guard
+            let minute = Float(self.slice(from: .minuteStart, to: .minuteEnd)),
+            let second = Float(self.slice(from: .secondStart, to: .secondEnd)),
+            let millisecond = Float(self.slice(from: .millisecond, to: .millisecond))
+        else {
+            return 0
+        }
+        
+        return (minute * 60) + second + (millisecond / 10)
+    }
+    
+    var lyrics: String {
+        self.slice(from: .lyricsStart)
     }
 }
