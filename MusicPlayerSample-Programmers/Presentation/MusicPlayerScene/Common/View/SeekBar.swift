2e1e0f8a0e8c5625ffaa42745cda4a4c91330fba
Jaewon <woin2ee@gmail.com>
2022-07-16 21:39:29 +0900

    feat : seek bar 터치시 확대 애니메이션 구현

--- MusicPlayerSample-Programmers/Presentation/MusicPlayerScene/Common/View/SeekBar.swift
+++ MusicPlayerSample-Programmers/Presentation/MusicPlayerScene/Common/View/SeekBar.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class SeekBar: UISlider {
     
+    let animateDuration: TimeInterval = 0.05
+    
     override func thumbRect(forBounds bounds: CGRect, trackRect rect: CGRect, value: Float) -> CGRect {
         return super.thumbRect(
             forBounds: bounds,
@@ -18,9 +20,36 @@ final class SeekBar: UISlider {
     }
     
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
+        setValueCorrespondingToLocation(of: touch)
+        increaseBounds()
+        return true
+    }
+    
+    override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
+        super.endTracking(touch, with: event)
+        makeOriginalSize()
+    }
+    
+    override func cancelTracking(with event: UIEvent?) {
+        super.cancelTracking(with: event)
+        makeOriginalSize()
+    }
+    
+    private func setValueCorrespondingToLocation(of touch: UITouch) {
         let width = self.frame.width
         let tapPoint = touch.location(in: self)
         self.value = self.maximumValue * Float(tapPoint.x / width)
-        return true
+    }
+    
+    private func increaseBounds() {
+        UIView.animate(withDuration: self.animateDuration) {
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 2.0)
+        }
+    }
+    
+    private func makeOriginalSize() {
+        UIView.animate(withDuration: self.animateDuration) {
+            self.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
     }
 }
